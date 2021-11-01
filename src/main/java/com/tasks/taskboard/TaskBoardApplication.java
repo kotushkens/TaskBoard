14abafc6bb0e9a42dc616af7737aa60344340203
kotushkens <theseverest@gmail.com>
2021-11-01 17:52:51 +0300

    TASK-1_init_repo

---
+++ src/main/java/com/tasks/taskboard/TaskBoardApplication.java
@@ -0,0 +1,13 @@
+package com.tasks.taskboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tasks/taskboard/TaskBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasks.taskboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
